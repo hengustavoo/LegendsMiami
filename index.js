eb0b8055289a901993b1a195bdfc04ce38544361
Gustavo Valério <gustavocontatohenrique@gmail.com>
2023-10-13 11:15:57 -0300

    att

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ window.addEventListener("scroll", () => {
     header.classList.toggle('scrolling', window.scrollY > 1080)
 })
 
-document.querySelector('.pri').addEventListener("onclick", () => {
-    let divs = document.querySelectorAll('.pri')
-    divs.forEach(e => {
-        if (e.classList.contains('active') == true) {
-            e.classList.remove('active')
-        }
-    })
-    document.querySelector('pri').classList.add('active')
-})
+// document.querySelector('.pri').addEventListener("onclick", () => {
+//     let divs = document.querySelectorAll('.pri')
+//     divs.forEach(e => {
+//         if (e.classList.contains('active') == true) {
+//             e.classList.remove('active')
+//         }
+//     })
+//     document.querySelector('pri').classList.add('active')
+// })
 
 function visible(x) {
     const divs = document.querySelectorAll('.sub')
