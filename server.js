aecfa5798e00e8adb4b4d994dcee43b4e01ffeeb
Gustavo Valério <gustavocontatohenrique@gmail.com>
2025-04-08 08:46:14 -0300

    att

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 3001
 
 app.use(express.static(path.join(__dirname, 'public')))
 
